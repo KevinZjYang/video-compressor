55c66f500ee191883d2808b7f5731578228bba00
KevinZjYang <kevin.zj.yang@outlook.com>
2026-03-14 21:22:53 +0800

    打包0.1.0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { useAppStore } from "./stores/app";
+import { open } from "@tauri-apps/plugin-shell";
 
 const store = useAppStore();
 
 onMounted(() => {
   store.initFfmpeg();
 });
+
+const openWebsite = async () => {
+  await open("https://apphub.yiun.top/");
+};
 </script>
 
 <template>
   <div class="app-container">
     <router-view />
+    <div class="footer">
+      <span class="brand">视频压缩器-K</span>
+      <span class="divider">|</span>
+      <a class="link" @click="openWebsite">官网: https://apphub.yiun.top/</a>
+    </div>
   </div>
 </template>
 
@@ -41,6 +51,37 @@ body {
   width: 100%;
   display: flex;
   flex-direction: column;
+}
+
+.footer {
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border-top: 1px solid #e4e7ed;
+  font-size: 12px;
+  color: #909399;
+  flex-shrink: 0;
+}
+
+.footer .brand {
+  font-weight: 500;
+  color: #606266;
+}
+
+.footer .divider {
+  margin: 0 8px;
+}
+
+.footer .link {
+  color: #409eff;
+  cursor: pointer;
+  text-decoration: none;
+}
+
+.footer .link:hover {
+  color: #66b1ff;
 }
 
 /* Element Plus 覆盖样式 */
